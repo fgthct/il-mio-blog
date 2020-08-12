4af6ed2460bdfdbf0ce801fc5cebd28992e824b7
fgthct <fgthct@gmail.com>
2020-08-12 13:06:29 +0200

    Ho cambiato l'HTML per questo sito.

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+
+def home_view(request):
+    return render(request, 'blog/home_view.html', {})
